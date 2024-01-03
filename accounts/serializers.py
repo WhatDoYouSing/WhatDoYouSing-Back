1e93c365db26180a43ef6f92424da7783dc11e21
김지은 <silver@kimjieun-ui-MacBookAir.local>
2024-01-03 00:04:01 -0500

    [feat]회원 관련 기능 추가

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import *
 
-class UserSerializer(serializers.ModelSerializer):
+class SignUpSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id','username','password','nickname']
@@ -23,7 +23,16 @@ class UserSerializer(serializers.ModelSerializer):
 
         return user
     
-class UserLoginSerializer(serializers.Serializer):
+    def update(self, validated_data):
+        password = validated_data.get("password",None)
+
+class ProfileChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields = ['id','profile']
+
+    
+class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=64)
     password = serializers.CharField(max_length=128, write_only=True)
 
@@ -48,5 +57,35 @@ class UserLoginSerializer(serializers.Serializer):
                 }
 
                 return data
-        else:
+        else: 
             raise serializers.ValidationError('존재하지 않는 사용자입니다.')
+        
+class UsernameUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','username']
+    
+    #def validate_username(self, value):
+    #    existing_user = User.objects.filter(nickname__iexact=value).first()
+    #    if existing_user:
+    #        raise serializers.ValidationError('이미 사용 중인 아이디입니다.')
+    #    return value
+
+class NicknameUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','nickname']
+    
+    def validate_nickname(self, value):
+        existing_user = User.objects.filter(nickname__iexact=value).first()
+        if existing_user:
+            raise serializers.ValidationError('이미 사용 중인 닉네임입니다.')
+        return value
+
+    
+class PasswordUpdateSerializer(serializers.Serializer):
+    new_password = serializers.CharField(max_length=128, write_only=True)
+    confirm_new_password = serializers.CharField(max_length=128, write_only=True)
+
+class UserConfirmSerializer(serializers.Serializer):
+    enter_password = serializers.CharField(max_length=128, write_only=True)
--- accounts/views.py
+++ accounts/views.py
@@ -15,7 +15,7 @@ from .serializers import *
 # Create your views here.
 
 class SignUpView(views.APIView):
-    serializer_class = UserSerializer
+    serializer_class = SignUpSerializer
 
     def post(self, request, format=None):
         serializer = self.serializer_class(data=request.data)
@@ -25,8 +25,26 @@ class SignUpView(views.APIView):
             return Response({'message':'회원가입 성공', 'data':serializer.data})
         return Response({'message':'회원가입 실패', 'error':serializer.errors})
     
+class ProfileChoiceView(views.APIView):
+    serializer_class = ProfileChoiceSerializer
+    
+    def get(self, request, format=None):
+        serializer = self.serializer_class(request.user)
+        return Response(serializer.data)
+
+    
+    def patch(self, request, format=None):
+        user = request.user
+        serializer = self.serializer_class(user, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': '프로필 지정 성공.', 'data': serializer.data}, status=HTTP_200_OK)
+        return Response({'message': '프로필 지정 실패.', 'data': serializer.errors}, status=HTTP_400_BAD_REQUEST)
+
+    
 class LoginView(views.APIView):
-    serializer_class = UserLoginSerializer
+    serializer_class = LoginSerializer
     
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
@@ -34,3 +52,107 @@ class LoginView(views.APIView):
         if serializer.is_valid():
             return Response({'message':'로그인 성공', 'data':serializer.data})
         return Response({'message':'로그인 실패', 'error':serializer.errors})
+    
+class DuplicateIDView(views.APIView):
+    def post(self, request):
+        username = request.data.get('username')
+
+        if User.objects.filter(username=username).exists():
+            response_data = {'duplicate':True}
+        else:
+            response_data = {'duplicate':False}
+        
+        return Response(response_data, status=HTTP_200_OK)
+    
+class ChangeUsernameView(views.APIView):
+    serializer_class = UsernameUpdateSerializer
+    
+    def get(self, request, format=None):
+        serializer = self.serializer_class(request.user)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = UsernameUpdateSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            user = request.user
+            new_username = serializer.validated_data['new_username']
+
+            # 새로운 아이디 설정
+            user.set_username(new_username)
+            user.save()
+
+            return Response({'message': '아이디 변경 성공.'}, status=HTTP_200_OK)
+        else:
+            return Response({'message': '올바르지 않은 데이터입니다.'}, status=HTTP_400_BAD_REQUEST)
+
+    
+class ChangePasswordView(views.APIView):
+    serializer_class = PasswordUpdateSerializer
+    def post(self, request, format=None):
+        serializer = PasswordUpdateSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            user = request.user
+            new_password = serializer.validated_data['new_password']
+            confirm_new_password = serializer.validated_data['confirm_new_password']
+
+            # 새로운 비밀번호 확인
+            if new_password != confirm_new_password:
+                return Response({'message': '새로운 비밀번호와 확인 비밀번호가 일치하지 않습니다.'}, status=HTTP_400_BAD_REQUEST)
+
+            # 새로운 비밀번호 설정
+            user.set_password(new_password)
+            user.save()
+
+            return Response({'message': '비밀번호가 성공적으로 변경되었습니다.'}, status=HTTP_200_OK)
+        else:
+            return Response({'message': '올바르지 않은 데이터입니다.'}, status=HTTP_400_BAD_REQUEST)
+        
+        
+class ChangeNicknameView(views.APIView):
+    serializer_class = NicknameUpdateSerializer
+    
+    def get(self, request, format=None):
+        serializer = self.serializer_class(request.user)
+        return Response(serializer.data)
+
+    
+    def patch(self, request, format=None):
+        user = request.user
+        serializer = self.serializer_class(user, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': '닉네임 변경 성공.', 'data': serializer.data}, status=HTTP_200_OK)
+        return Response({'message': '닉네임 변경 실패.', 'data': serializer.errors}, status=HTTP_400_BAD_REQUEST)
+
+class UserDeleteView(views.APIView):
+    serializer_class=UserConfirmSerializer
+    def post(self, request):
+        serializer = UserConfirmSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = request.user
+            enter_password = serializer.validated_data['enter_password']
+
+            if not user.check_password(enter_password):
+                return Response({'message': '접근 실패, 비밀번호가 옳지 않습니다.', 'access':False}, status=HTTP_400_BAD_REQUEST)
+            else:
+                #user = request.user
+                user.delete()
+                return Response({'message': '접근 성공. 회원 탈퇴가 완료되었습니다.', 'access':True}, status=HTTP_200_OK)
+    
+class UserAccessView(views.APIView):
+    serializer_class=UserConfirmSerializer
+    def post(self, request):
+        serializer = UserConfirmSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = request.user
+            enter_password = serializer.validated_data['enter_password']
+
+            if not user.check_password(enter_password):
+                return Response({'message': '접근 실패, 비밀번호가 옳지 않습니다.', 'access':False}, status=HTTP_400_BAD_REQUEST)
+            else:
+                return Response({'message': '접근 성공.', 'access':True}, status=HTTP_200_OK)
