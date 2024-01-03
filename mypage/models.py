41afdbd33f38e779abb1f6505b00b1a977a3f086
김지은 <silver@kimjieun-ui-MacBookAir.local>
2024-01-03 12:55:06 -0500

    [feat]마이페이지

--- mypage/models.py
+++ mypage/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
+
+class MyPage(models.Model):
+    user = models.ForeignKey("accounts.User", null=True, on_delete=models.CASCADE)
--- mypage/urls.py
+++ mypage/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+    path('profile/', ProfileView.as_view()),
 ]
