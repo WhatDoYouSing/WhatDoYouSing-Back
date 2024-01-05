453d99ee1c09fa05753260f22923aa249599d8ca
김지은 <silver@kimjieun-ui-MacBookAir.local>
2024-01-04 19:12:41 -0500

    [fix]마이페이지

--- mypage/serializers.py
+++ mypage/serializers.py
@@ -7,3 +7,20 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = ['id','username','nickname','profile']
 
 #이후 각 기능별 시리얼라이저 작성 필요
+#수정필요!!
+        
+class ScrapCollectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MyPage
+
+class SingsCollectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MyPage
+
+class CommentsCollectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MyPage
+
+class EmotionsCollectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MyPage
