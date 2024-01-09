07efcf686e734c333c7b34ddba131740448d4dd6
김지은 <silver@kimjieun-ui-MacBookAir.local>
2024-01-09 18:15:07 -0500

    [fix]scrap

--- posts/serializers.py
+++ posts/serializers.py
@@ -38,7 +38,7 @@ class PostSerializer(FunctionMixin, serializers.ModelSerializer):
     #comment_count = serializers.SerializerMethodField()
     author_nickname = serializers.SerializerMethodField()
     author_profile = serializers.SerializerMethodField()
-    is_scraped = serializers.SerializerMethodField()
+    #is_scraped = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
@@ -61,9 +61,9 @@ class PostSerializer(FunctionMixin, serializers.ModelSerializer):
 
         read_only_fields = ["author"]
 
-        def get_is_scraped(self, obj):
-            user = self.context['request'].user
-            return user in obj.scrap.all()
+        #def get_is_scraped(self, obj):
+        #    user = self.context['request'].user
+        #    return user in obj.scrap.all()
 
 class EmotionSerializer(serializers.ModelSerializer):
     class Meta:
