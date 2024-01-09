adecb6b526cfc71b3a5211def0b9f5229ea9cf6b
yoonseoah <emma.yoon2002@gmail.com>
2024-01-09 21:33:54 +0900

    [fix] Recomments views.py, urls.py 재수정

--- comments/urls.py
+++ comments/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("<int:post_pk>/", CommentView.as_view()),
     path("del/<int:comment_pk>/", CommentDelView.as_view()),
     path("<int:comment_pk>/likes/", CommentLikeView.as_view()),
-    path("<int:post_pk>/<int:comment_pk>/recomments/", RecommentView.as_view()),
+    path("<int:comment_pk>/recomments/", RecommentView.as_view()),
     path("del/recomments/<int:recomment_pk>/", RecommentDelView.as_view()),
     path("<int:comment_pk>/recomments/<int:recomment_pk>/relikes/", RecommentLikeView.as_view()),
 ]
