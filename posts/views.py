83b061ac2cc4decfe64a705abc48d26ee6fa5184
yoonseoah <emma.yoon2002@gmail.com>
2024-01-11 03:43:56 +0900

    [fix] 로그인안했을 때 postadd 수정

--- posts/views.py
+++ posts/views.py
@@ -26,8 +26,7 @@ class PostListView(views.APIView):
 
 class PostAddView(views.APIView):
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]  # Import IsAuthenticated
-
+    #permission_classes = [IsAuthenticated]  
 
     def post(self, request, format=None):  # 게시글 작성 POST 메소드
         if not request.user.is_authenticated:  # Check if the user is not authenticated
