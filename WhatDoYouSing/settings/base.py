e9d676992bc6627c045caad18c03747c80e09720
김지은 <silver@kimjieun-ui-MacBookAir.local>
2024-01-16 12:29:29 -0500

    [fix]emoji

--- WhatDoYouSing/settings/base.py
+++ WhatDoYouSing/settings/base.py
@@ -145,6 +145,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+            "charset": "utf8mb4",
         },
     },
 ]
